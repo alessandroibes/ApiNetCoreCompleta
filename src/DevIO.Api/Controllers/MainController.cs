1fbddf272a75b8a342088187eec16f9450d8fa39
Alessandro Oliveira <alessandro.ibes@gmail.com>
2022-01-10 15:08:39 -0300

    Adicionado Controller de Fornecedores

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validação de notificações de erro
+        // validação de ModelState
+        // validação da operação de negócio
+    }
+}
